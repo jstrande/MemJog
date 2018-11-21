6e51339e4654e1abe6d3b47d4475ff13b726f352
Jon Strande <jonstrande@jons-macbook.local>
2018-11-21 14:12:38 -0500

    Changes for MVC and added ToDoItem

---
+++ MemJog/DataModel/ToDoItem.swift
@@ -0,0 +1,22 @@
+//
+//  ToDoItem.swift
+//  MemJog
+//
+//  Created by Jon Strande on 11/21/18.
+//  Copyright © 2018 Jon Strande. All rights reserved.
+//
+
+import Foundation
+
+class ToDoItem {
+ 
+    var toDoItemTitle: String = "";
+    var isDone: Bool = false;
+    
+    init() {
+        //do any set up here
+    }
+    
+
+}
+
--- MemJog/ToDoListControllers/ToDoListViewController.swift
+++ MemJog/ToDoListControllers/ToDoListViewController.swift
@@ -10,24 +10,57 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    var itemArray = ["Clean House", "Learn Swift", "Book Flight", "Cook Tacos"]
+    //var itemArray = ["Clean House", "Learn Swift", "Book Flight", "Cook Tacos"]
+    var itemArray = [ToDoItem]()
+    
     
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: "ToDoList") as? [String] {
+        let newItem = ToDoItem()
+        newItem.toDoItemTitle = "Clean the house";
+        newItem.isDone = false
+        itemArray.append(newItem);
+        
+        let newItem2 = ToDoItem()
+        newItem2.toDoItemTitle = "Learn Swift";
+        newItem2.isDone = true;
+        itemArray.append(newItem2);
+        
+        let newItem3 = ToDoItem()
+        newItem3.toDoItemTitle = "Book Flight";
+        newItem3.isDone = false
+        itemArray.append(newItem3);
+        
+        if let items = defaults.array(forKey: "ToDoList") as? [ToDoItem] {
             itemArray = items;
         }
+        
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count;
     }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row];
+        let item = itemArray[indexPath.row];
+        cell.textLabel?.text = item.toDoItemTitle;
+       
+        //THIS ONE LINE
+        //cell.accessoryType = item.isDone == true ? .checkmark : .none;
+        
+        //REPLACES THESE LINES
+        if item.isDone == true {
+            cell.accessoryType = .checkmark;
+        }
+        else {
+            cell.accessoryType = .none;
+        }
+        
         return cell;
     }
     
@@ -35,12 +68,8 @@ class ToDoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
-        else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].isDone = !itemArray[indexPath.row].isDone;
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true);
     }
     //override func tabl
@@ -54,11 +83,29 @@ class ToDoListViewController: UITableViewController {
         
         let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            
+            print("CREATING NEW ITEM")
             //what happens when the person clicks the add item alert thing
-            self.itemArray.append(textField.text!)
+            let newItemX = ToDoItem();
+            newItemX.toDoItemTitle = textField.text!
+            print("NEW ITEM CREATED: " + newItemX.toDoItemTitle);
+            
+            print("APPENDING TO ARRAY")
+            self.itemArray.append(newItemX)
+            print("SETTING INTO DEFAULS")
+            
+            /******************************************
+             //
+             // I have a bug here - it crashes when I set this 
+             //
+             //
+ 
+            */
+            
             
             self.defaults.set(self.itemArray, forKey: "ToDoList");
-            
+            print("RELOADING TABLE VIEW")
             self.tableView.reloadData();
             
         }
