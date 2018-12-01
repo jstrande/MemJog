809af8706e8f60c00abc6862d36f64eb7ac1e60e
Jon Strande <jonstrande@2603-9000-8f90-e700-8d4b-8926-6b90-8c66.res.spectrum.com>
2018-12-01 10:57:33 -0500

    Updated for categories

--- MemJog/ToDoListControllers/CategoryViewController.swift
+++ MemJog/ToDoListControllers/CategoryViewController.swift
@@ -11,24 +11,92 @@ import CoreData
 
 class CategoryViewController: UITableViewController {
 
+    
+    var categoryArray = [Category]()
+    let context = (UIApplication.shared.delegate as! AppDelegate) .persistentContainer.viewContext;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        loadCategories();
         // Do any additional setup after loading the view.
     }
     
     
     //MARK: - TableView Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categoryArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        let category = categoryArray[indexPath.row];
+        cell.textLabel?.text = category.name;
+        cell.accessoryType = .disclosureIndicator;
+        return cell;
+    }
     
     //MARK: - TableView Delegate Methods
     
-    //MARK: - Data Manipulation Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //this would be what would happen if I touch an item
+        //to go to the detail screen to see the list
+        performSegue(withIdentifier: "goToItems", sender: self)
+        
+        //give it better animation
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //print(segue.identifier!)
+        let destinationVC = segue.destination as! ToDoListViewController
+        
+        if let indexPath = tableView.indexPathForSelectedRow {
+            destinationVC.selectedCategory = categoryArray[indexPath.row]
+            
+        }
+        
+    }
     
     //MARK: - Add new categories
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField();
+        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert);
+        let action = UIAlertAction(title: "Add Category", style: .default) { (action) in
+            let newCategory = Category(context: self.context);
+            newCategory.name = textField.text!
+            self.categoryArray.append(newCategory)
+            self.saveCategory()
+        }
+        alert.addTextField(configurationHandler: { (alertTextField) in alertTextField.placeholder = "Add New Category"
+            textField = alertTextField;
+            
+            })
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil);
+        
     }
 
+    
+    //MARK: - Data Manipulation Methods
+    func saveCategory () {
+        do {
+            try context.save();
+        }
+        catch {
+            print("inside catch of saveCategory(), \(error)")
+        }
+        self.tableView.reloadData();
+    }
+    func loadCategories () {
+        do {
+            let request : NSFetchRequest<Category> = Category.fetchRequest();
+            categoryArray = try context.fetch(request);
+        }
+        catch {
+            print("Error loading categories when fetching \(error)");
+        }
+        tableView.reloadData();
+    }
     
 }
--- MemJog/ToDoListControllers/ToDoListViewController.swift
+++ MemJog/ToDoListControllers/ToDoListViewController.swift
@@ -14,24 +14,32 @@ class ToDoListViewController: UITableViewController {
     //var itemArray = ["Clean House", "Learn Swift", "Book Flight", "Cook Tacos"]
     var itemArray = [Item]()
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext;
-    
-    //Note, the file name here - can have multiple plist files...
-    //one plist for each category of to do items
-    //let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("MemJogItems.plist");
-    
-    //let defaults = UserDefaults.standard
+    var selectedCategory : Category? {
+        didSet {
+            loadItems();
+        }
+    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask);
+        //let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask);
+        //print(dataFilePath);
         
-        print(dataFilePath);
-        
-        loadItems();
+        //loadItems();
+        let navigationTitle = selectedCategory?.name;
+        if navigationTitle != nil {
+            navigationItem.title = navigationTitle;
+        }
+        else {
+            navigationItem.title = "Reminders";
+        }
         
     }
 
+    //MARk: - Tableview Datasource Methods
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count;
     }
@@ -41,12 +49,7 @@ class ToDoListViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         let item = itemArray[indexPath.row];
         cell.textLabel?.text = item.title;
-       
-        
-        //THIS ONE LINE
-        //cell.accessoryType = item.isDone == true ? .checkmark : .none;
-        
-        //REPLACES THESE LINES
+    
         if item.done == true {
             cell.accessoryType = .checkmark;
         }
@@ -82,13 +85,11 @@ class ToDoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             
-            
-            //let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext;
             //this is what happens when the person clicks the add item alert thing
             let newItemX = Item(context: self.context);
             newItemX.title = textField.text!
             newItemX.done = false;
-
+            newItemX.parentCategory = self.selectedCategory;
             self.itemArray.append(newItemX)
             self.saveItems()
             
@@ -116,16 +117,31 @@ class ToDoListViewController: UITableViewController {
             try context.save();
         }
         catch {
-            print("inside catch of encoder.encode, \(error)")
+            print("inside catch of saveItems(), \(error)")
         }
         self.tableView.reloadData();
     }
     
     //MARK: - LOAD ITEMS
  
-    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
         //print("printing request")
         //print(request)
+       
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+        
+        if let additionalPredicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additionalPredicate])
+        }
+        else {
+            request.predicate = categoryPredicate;
+        }
+        
+        /*
+        let compoundPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, predicate])
+        request.predicate = compoundPredicate;
+         */
+        
         do {
             itemArray = try context.fetch(request)
         }
@@ -134,32 +150,19 @@ class ToDoListViewController: UITableViewController {
         }
         tableView.reloadData();
     }
-    /*
-    func loadItems() {
-        let request : NSFetchRequest<Item> = Item.fetchRequest()
-        do {
-            itemArray = try context.fetch(request)
-        }
-        catch {
-            print("Error loading items when fetching \(error)");
-        }
-        tableView.reloadData();
-    }
-    */
-    
-
 
     
 }
 
 //MARK: - SEARCH BAR METHODS
 extension ToDoListViewController: UISearchBarDelegate {
+    
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         let request : NSFetchRequest<Item> = Item.fetchRequest();
-        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!);
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!);
         request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)];
-
-        loadItems(with: request);
+        
+        loadItems(with: request, predicate: predicate);
 
         
     }
